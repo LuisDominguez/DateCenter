87929ea38b337121518006920d645a964737cce9
luis dominguez <luis_dmz_92@hotmail.com>
2015-02-06 10:37:36 -0600

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,12 +25,16 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  #config.assets.debug = true
+  config.assets.debug = false #VPS
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+
+  config.log_level = :info #VPS
+  #config.log_level = :debug
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
